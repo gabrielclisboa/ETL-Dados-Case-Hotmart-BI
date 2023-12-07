139dbfa3cf885b8c71111893b261491ba4956ef2
Gabriel Campos <gabriel.c.lisboa>
2023-12-07 02:51:55 -0300

    fix: correção na geração de endereço e adicionado a geração de datas fake

--- pipeline.py
+++ pipeline.py
@@ -42,13 +42,14 @@ def transform(df: DataFrame) -> DataFrame:
     # Transformação para a Tabela Dimensão Produtor Afiliado
 
     # Z-score
-    df['purchase_value'] = (df['purchase_value'] - 60) / 100
     df['purchase_value'] = df['purchase_value'].apply(lambda x: x if x >= 0 else -1 * x)
+    df['purchase_value'] = round(df['purchase_value'] * 100 + 60, 2)
+
 
     dim_afiliado = df[['affiliate_id']].drop_duplicates()
     dim_afiliado['name'] = dim_afiliado['affiliate_id'].apply(lambda x: fake.name())
     dim_afiliado['sales_history'] = df.groupby('affiliate_id').size().reindex(dim_afiliado['affiliate_id']).fillna(0).astype(int).values
-    dim_afiliado['creation_date'] = pd.to_datetime('now')
+    dim_afiliado['creation_date'] = dim_afiliado['affiliate_id'].apply(lambda x:pd.to_datetime (fake.date_time_between(start_date='-10y', end_date='-7y')))
     dim_afiliado['status'] = True
     dim_afiliado['email'] = dim_afiliado['affiliate_id'].apply(lambda x: fake.email())
 
@@ -64,7 +65,7 @@ def transform(df: DataFrame) -> DataFrame:
     dim_produtor['email'] = dim_produtor['producer_id'].apply(lambda x: fake.email())
     dim_produtor['sales_history'] = vendas_do_produtor['producer_id'].value_counts().reindex(dim_produtor['producer_id']).fillna(0).astype(int).values
     dim_produtor['status'] = True
-    dim_produtor['creation_date'] = pd.to_datetime('now')
+    dim_produtor['creation_date'] = dim_produtor['producer_id'].apply(lambda x: pd.to_datetime (fake.date_time_between(start_date='-10y', end_date='-7y')))
     dim_produtor['rating'] = dim_produtor['producer_id'].apply(lambda x: round(random.uniform(0, 5), 1))
 
     # Transformação para a Tabela Fato Venda
@@ -78,17 +79,17 @@ def transform(df: DataFrame) -> DataFrame:
     # Transformação para a Tabela Dimensão Comprador
     dim_comprador = df[['buyer_id']].drop_duplicates()
     dim_comprador['name'] = dim_comprador['buyer_id'].apply(lambda x: fake.name()) 
-    dim_comprador['address'] = dim_comprador['buyer_id'].apply(lambda x: fake.address())  
+    dim_comprador['address'] = dim_comprador['buyer_id'].apply(lambda x:  fake.address().replace('\n', ' ').strip())  
     dim_comprador['email'] = dim_comprador['buyer_id'].apply(lambda x: fake.email())
     dim_comprador['age'] = dim_comprador['buyer_id'].apply(lambda x: random.randint(18, 70)) 
     dim_comprador['status'] = True  
-    dim_comprador['creation_date'] = pd.to_datetime('now')
+    dim_comprador['creation_date'] = dim_comprador['buyer_id'].apply(lambda x: pd.to_datetime (fake.date_time_between(start_date='-10y', end_date='-7y')))
 
     # Transformação para a Tabela Dimensão Segmento do Usuário
     dim_segmento_usuario = pd.DataFrame(index=df.index)  
     dim_segmento_usuario['user_segment_id'] = df.index + 1
     dim_segmento_usuario['category_name'] = ''  
-    dim_segmento_usuario['date_sale'] = pd.to_datetime('now')
+    dim_segmento_usuario['date_sale'] = dim_segmento_usuario['user_segment_id'].apply(lambda x: pd.to_datetime (fake.date_time_between(start_date='-10y', end_date='-7y'))) 
     dim_segmento_usuario['last_category'] = 0  
     dim_segmento_usuario['current_category'] = ''
 
